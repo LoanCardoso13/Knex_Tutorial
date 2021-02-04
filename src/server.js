3bdc3ffb6d215383553008c4fc9bd06b7447e987
Loan Cardoso <jow13_122@yahoo.com>
2021-02-04 14:23:40 -0300

    Finishing CRUD

--- src/server.js
+++ src/server.js
@@ -3,7 +3,14 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
+
+// catch all
+app.use((error,request,response,next) => {
+    response.status(error.status || 500);
+    response.json( {error: error.message} );
+})
 
 app.listen(3333, () => {
     console.log('server is running');
